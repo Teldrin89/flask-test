277cfb5a06ac5f784025b0f5abeb743fb6936d85
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2024-10-20 22:20:54 +0200

    adding index route that gets all blog posts

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,3 +6,18 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    '''
+    index view function with list of posts
+    '''
+    db = get_db() # get database
+    # read posts from database with set of parameters
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ODER BY created DESC'
+    ).fetchall()
+
+    return render_template('blog/index.html', posts=posts)
